de7858285b6d6d000567deba2f3d288bdeab3e8d
saberfig <1023736798@qq.com>
2019-04-29 10:41:25 +0800

    文件整理-删除

---
+++ Web_Game/src/com/bupt/client/Client.java
@@ -0,0 +1,18 @@
+package com.bupt.client;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+	public static void main(String[] args) throws IOException{
+		//创建Socket对象
+		Socket client=new Socket("localhost",8080);
+		//创建发送的线程类对象
+		Send send=new Send(client);
+		//创建接收的线程类对象
+		Receive receive=new Receive(client);
+		//启动线程
+		send.start();
+		receive.start();
+	}
+}
---
+++ Web_Game/src/com/bupt/client/Receive.java
@@ -0,0 +1,36 @@
+package com.bupt.client;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Receive extends Thread {
+	
+	//接收数据的数据流
+	DataInputStream dis;
+	private boolean flag=true;
+	public Receive(Socket client) {
+		try {
+			dis=new DataInputStream(client.getInputStream());
+		} catch (IOException e) {
+			flag=false;
+			CloseUtils.closeAll(dis,client);
+		}
+	}
+	private String getMessage(){
+		String str="";
+		try {
+			str=dis.readUTF();
+		} catch (IOException e) {
+			flag=false;
+			CloseUtils.closeAll(dis);
+		}
+		return str;
+	}
+	@Override
+	public void run() {
+		while(flag) {
+			System.out.println(this.getMessage());
+		}
+	}
+}
